c0a4d4e84163bb5364d4a53ad1f8045384d86784
Petty <pettwil1131@gmail.com>
2021-06-17 18:23:22 -0500

    Creates vue app, adds vue-slick-carousel and adjusts properties

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="carousel">
+      <VueSlickCarousel v-bind="settings">
+        <div><h1>1</h1></div>
+        <div><h1>2</h1></div>
+        <div><h1>3</h1></div>
+        <div><h1>4</h1></div>
+        <div><h1>5</h1></div>
+        <div><h1>6</h1></div>
+      </VueSlickCarousel>
+    </div>
   </div>
 </template>
 
+<style>
+.carousel {
+  background: #eeeeee;
+  max-width: 50%;
+  margin: auto;
+  padding: 50px;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+// optional style for arrows & dots
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      settings: {
+        arrows: true,
+        dots: true,
+        dotsClass: "slick-dots",
+        edgeFriction: 0.75,
+        infinite: true,
+        speed: 250,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        pauseOnDotsHover: true,
+        draggable: true,
+        focusOnSelect: true,
+      },
+    };
   },
+  name: "MyComponent",
+  components: { VueSlickCarousel },
+
+  methods: {},
 };
 </script>
